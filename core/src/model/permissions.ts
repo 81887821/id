2462bdd2d84dbde9ec066815958bf7582b7be023
skystar-p <usezmap@gmail.com>
2018-09-13 15:59:58 +0900

    hosts.ts test

--- core/src/model/permissions.ts
+++ core/src/model/permissions.ts
@@ -47,8 +47,11 @@ export default class Permissions {
   }
 
   public async checkUserHavePermission(client: PoolClient, userIdx: number, permissionIdx: number): Promise<boolean> {
-    const userReachableGroups = Array.from(await this.model.users.getUserReachableGroups(client, userIdx))
     const permissionRequirements = await this.getAllPermissionRequirements(client, permissionIdx)
+    if (permissionRequirements.length === 0) {
+      return false
+    }
+    const userReachableGroups = Array.from(await this.model.users.getUserReachableGroups(client, userIdx))
 
     for (const pr of permissionRequirements) {
       if (!userReachableGroups.includes(pr)) {
